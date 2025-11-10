ac4723cf79c94160f2e1b5a44ded5e9286219fc2
PubbarajuJahnavi <11249A299@kanchiuniv.ac.in>
2025-11-09 23:23:19 -0800

    Add files via upload

---
+++ user defined namespace.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+namespace college{
+    void display(){
+        cout<<"welcome to the university portal"<<endl;
+    }
+}
+int main(){
+    college::display();
+    return 0;
+
+}
